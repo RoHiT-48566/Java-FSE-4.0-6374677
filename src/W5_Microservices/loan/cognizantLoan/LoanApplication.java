33edf8196875078637e332e25118461648538c1e
RoHiT-48566 <m.rohit48566@gmail.com>
2025-07-17 18:04:18 +0530

    Completed Week-5 exercises

---
+++ src/W5_Microservices/loan/cognizantLoan/LoanApplication.java
@@ -0,0 +1,9 @@
+package W5_Microservices.loan.cognizantLoan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+public class LoanApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApplication.class, args);
+    }
+}
